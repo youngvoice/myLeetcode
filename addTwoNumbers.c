892039a0e93b4d18c3085a6bde53229595c994b5
xjk <2459169377@qq.com>
2019-10-16 20:11:30 +0800

    2 Add Two Numbers
    section edition

--- addTwoNumbers.c
+++ addTwoNumbers.c
@@ -8,71 +8,95 @@
 
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-        
-        struct ListNode* l3 = (struct ListNode*)malloc(sizeof(struct ListNode));
-        struct ListNode *p;
-        int carry = 0;
-        p = l3;
-        l3->next = NULL;       
-        l3->val = l1->val + l2->val;
-        if (p->val >= 10) {
-                carry = p->val/10;
-                p->val = p->val%10;
-            }
+		struct ListNode *l3, *p;
+		l3 = p = NULL;
+		int carry = 0;
 
-        
-        while (l1->next != NULL && l2->next != NULL)
-        {
-            l1 = l1->next;
-            l2 = l2->next;
-            p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-            p = p->next;
-            p->next = NULL;
-            p->val = l1->val + l2->val + carry;
-            carry = 0;
-            if (p->val >= 10) {
-                carry = p->val/10;
-                p->val = p->val%10;
-            }
-        }
-        if (l1->next != NULL)
-        while (l1->next != NULL)
-        {
-            l1 = l1->next;
-            p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-            p = p->next;
-            p->next = NULL;
-            p->val = l1->val + carry;
-            carry = 0;
-            if (p->val >= 10) {
-                carry = p->val/10;
-                p->val = p->val%10;
-            }
+		while (l1 && l2) {
+				if (!p) {
+						p = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next = NULL;
+						l3 = p;
+				}
+				else {
+						p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next->next = NULL;
+						p = p->next;
+				}
 
-        }
-        if (l2->next != NULL)
-        while (l2->next != NULL)
-        {
-            l2 = l2->next;
-            p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-            p = p->next;
-            p->next = NULL;
-            p->val = l2->val + carry;
-            carry = 0;
-            if (p->val >= 10) {
-                carry = p->val/10;
-                p->val = p->val%10;
-            }
-
-        }
-        if (carry != 0) {
-            p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
-            p = p->next;
-            p->next = NULL;
-            p->val = carry;
-        }
+				p->val = l1->val + l2->val + carry;
+				carry = 0;
+				if (p->val >= 10) {
+						carry = p->val / 10;
+						p->val = p->val % 10;
+				}
 
 
-        return l3;
-            
+				l1 = l1->next;
+				l2 = l2->next;
+		}
+		while (l2) {
+				if (!p) {
+						p = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next = NULL;
+						l3 = p;
+				}
+				else {
+						p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next->next = NULL;
+						p = p->next;
+				}
+
+				p->val = l2->val + carry;
+				carry = 0;
+				if (p->val >= 10) {
+						carry = p->val / 10;
+						p->val = p->val % 10;
+				}
+
+
+				l2 = l2->next;
+
+		}
+
+		while (l1) {
+				if (!p) {
+						p = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next = NULL;
+						l3 = p;
+				}
+				else {
+						p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next->next = NULL;
+						p = p->next;
+				}
+
+				p->val = l1->val + carry;
+				carry = 0;
+				if (p->val >= 10) {
+						carry = p->val / 10;
+						p->val = p->val % 10;
+				}
+
+
+				l1 = l1->next;
+
+		}
+
+		if (carry != 0) {
+				if (!p) {
+						p = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next = NULL;
+						l3 = p;
+				}
+				else {
+						p->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+						p->next->next = NULL;
+						p = p->next;
+				}
+
+				p->val = carry;
+		}
+
+		return l3;
 }
