1936339624f3be7db632c40df520914868f1b120
xjk <2459169377@qq.com>
2019-11-19 22:52:01 +0800

    simple and direct

--- findMaxConsecutiveOnes.c
+++ findMaxConsecutiveOnes.c
@@ -1,59 +1,15 @@
-#define OUT 0
-#define IN 1
 int findMaxConsecutiveOnes(int* nums, int numsSize)
 {
-		int i, j;
-		int state = OUT;
-		int max_count = 0;
-		i = 0;
-		int p, q;
+		int i, max_count, count;
+		max_count = 0;
+		count = 0;
 		for (i = 0; i < numsSize; i++) {
-				if ((nums[i] == 1) && ((state == OUT) || (i == 0))) {
-						p = i;
-						state = IN;
-
-				}
-
-				if (((nums[i] == 0) || (i == (numsSize - 1))) && (state == IN)) {
-						if (i == (numsSize - 1) && (nums[i] == 1))
-								i++;
-						q = i;
-						state = OUT;
-						if ((q - p) > max_count)
-								max_count = q - p;
-				}
+				if (nums[i] == 1)
+						count++;
+				else 
+						count = 0;
+				if (count > max_count)
+						max_count = count;
 		}
 		return max_count;
-
 }
-/*
-int main()
-{
-		int numsSize = 1;
-		int nums[1];
-		int i, j;
-		int state = OUT;
-		int max_count = 0;
-		i = 0;
-		int p, q;
-		nums[0] = 1;
-		for (i = 0; i < numsSize; i++) {
-				if ((nums[i] == 1) && ((state == OUT) || (i == 0))) {
-						p = i;
-						state = IN;
-
-				}
-
-				if (((nums[i] == 0) || (i == (numsSize - 1))) && (state == IN)) {
-						if (i == (numsSize - 1))
-								i++;
-						q = i;
-						state = OUT;
-						if ((q - p) > max_count)
-								max_count = q - p;
-				}
-		}
-		return max_count;
-
-}
-*/
