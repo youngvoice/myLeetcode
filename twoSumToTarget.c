b5d80c54474fc04a81b59665b84b49ede72ac9fc
xjk <2459169377@qq.com>
2019-11-19 20:47:21 +0800

    167. add the consideration of sort

--- twoSumToTarget.c
+++ twoSumToTarget.c
@@ -6,17 +6,23 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize)
 		int *ret = (int *)malloc(2*sizeof(int));
 		*returnSize = 2;
 		int flag = 0;
-		int i, j;
-		for (i = 0; i < numbersSize; i++) {
-				for (j = numbersSize - 1; i < j; j--) {
-						if ((numbers[i] + numbers[j]) == target)
-						{
-								flag = 1;
-								break;
-						}
-				}
-				if (flag)
+		int i, j, sum;
+
+		i = 0;
+		j = numbersSize - 1;
+		while (i < j) {
+				sum = numbers[i] + numbers[j];
+				if (sum == target) {
+						flag = 1;
 						break;
+				}
+				else {
+						if (sum < target) 
+								i++;
+						else 
+								j--;
+				}
+
 		}
 		ret[0] = i + 1;
 		ret[1] = j + 1;
