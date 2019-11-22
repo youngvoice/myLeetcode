d820e88cde21b5315df88fac40099ce1893f3e4a
xjk <2459169377@qq.com>
2019-11-22 21:53:44 +0800

    reference official method

--- minSubArrayLen.c
+++ minSubArrayLen.c
@@ -2,18 +2,19 @@ int minSubArrayLen(int s, int* nums, int numsSize)
 {
 		int i,j,k,sum;
 		int len = INT_MAX;
+
+		int left = 0;
 		sum = 0;
-		for (i = 0; i < numsSize; i++)
-				for (j = i; j < numsSize; j++) {
-						sum = 0;
-						for (k = i; k <= j; k++) {
-								sum += nums[k];
-								if (sum >= s) {
-										if (j-i+1 < len)
-												len = j-i+1;
-								}
-						}
+		for (i = 0; i < numsSize; i++) {
+				sum += nums[i];
+				while (sum >= s) {
+						if (i + 1 - left < len)
+								len = i + 1 - left;
+						sum -= mums[left];
+						left++;
+						//sum -= nums[left++];
 				}
+		}
 		return (len == INT_MAX) ? 0: len;
 
 }
