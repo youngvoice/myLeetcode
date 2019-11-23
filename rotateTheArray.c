94480985fa1fbb06d60d4a0240dc7b525f33829d
xjk <2459169377@qq.com>
2019-11-23 21:58:40 +0800

    one time, one step

--- rotateTheArray.c
+++ rotateTheArray.c
@@ -1,6 +1,19 @@
 void rotate(int* nums, int numsSize, int k)
 {
 
+
+		int i, j, temp, previous;
+		for (j = 0; j < k; j++) {
+				previous = nums[numsSize - 1];
+				for (i = 0; i < numsSize; i++) {
+						temp = nums[i];
+						nums[i] = previous;
+						previous = temp;
+
+				}
+		}
+
+		/*
 		int i, j, temp;
 		for (j = 0; j < k; j++)
 		for (i = numsSize - 1; i > 0; i--) {
@@ -8,6 +21,7 @@ void rotate(int* nums, int numsSize, int k)
 				nums[i] = nums[i - 1];
 				nums[i - 1] = temp;
 		}
+		*/
 
 }
 
