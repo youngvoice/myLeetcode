854197c017355616c3e4fa474317086e03e07d93
xjk <2459169377@qq.com>
2019-11-27 21:32:52 +0800

    first store nonzeros, and then store zeros

--- moveZeros.c
+++ moveZeros.c
@@ -1,3 +1,4 @@
+/*
 void swap(int i, int j, int *nums);
 void moveZeroes(int* nums, int numsSize)
 {
@@ -17,28 +18,6 @@ void moveZeroes(int* nums, int numsSize)
 				}
 		}
 }
-/*
-int nums[] = {0, 3, 12};
-int numsSize = 3;
-void main()
-{
-		int i,j,k,t;
-		for (i = numsSize - 1; i >= 0; i--) {
-				if (nums[i] != 0) {
-						k = i;
-						break;
-				}
-		}
-		for (i = k - 1; i >= 0; i--) {
-				if (nums[i] == 0) {
-						j = i;
-						for (t = 0; t < k - j; t++)
-								swap(j+t, j+1+t, nums);
-						k--;
-				}
-		}
-}
-*/
 
 void swap(int i, int j, int *nums)
 {
@@ -46,4 +25,21 @@ void swap(int i, int j, int *nums)
 		temp = nums[i];
 		nums[i] = nums[j];
 		nums[j] = temp;
+}
+*/
+
+void moveZeroes(int* nums, int numsSize)
+{
+		int *retNums;
+		int i, j;
+		j = 0;
+		retNums = (int *)malloc(numsSize*sizeof(int));
+		for (i = 0; i < numsSize; i++)
+				if (nums[i] != 0)
+						retNums[j++] = nums[i];
+		for (i = 0; i < numsSize; i++)
+				if (nums[i] == 0)
+						retNums[j++] = nums[i];
+		for (i = 0; i < numsSize; i++)
+				nums[i] = retNums[i];
 }
