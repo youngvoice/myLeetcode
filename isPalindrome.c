d0c95441f301cefdb32aa4c66508650eeb998854
xjk <2459169377@qq.com>
2019-12-08 22:18:40 +0800

    first revision

--- isPalindrome.c
+++ isPalindrome.c
@@ -8,49 +8,34 @@
 
 
 bool isPalindrome(struct ListNode* head){
-		int len = 0;
-		int mid, mid_left, mid_right;
-		struct ListNode *cur = head;
-		struct ListNode *mid_leftp, *mid_rightp;
-		int i;
-		if (cur == NULL)
-				return true;
+		struct ListNode *buffer, *cur, *t;
+		int ct = 0, len = 0;
+		cur = head;
+
+		buffer = NULL;
 		while (cur) {
 				len++;
+				t = (struct ListNode *)malloc(sizeof(struct ListNode));
+				t->next = buffer;
+				t->val = cur->val;
+				buffer = t;
 				cur = cur->next;
 		}
 
-
-		mid = len % 2;
-		if (mid == 0) {
-				mid_left = len / 2;
-				mid_right = mid_left + 1;
+		if (len % 2 == 0) {
+				ct = len / 2;
 		}
-		else {
-				mid_left = len / 2;
-				mid_right = mid_left + 2;
-		}
-
+		else
+				ct = (len - 1) / 2;
 		cur = head;
-		i = 0;
-		while (cur) {
-				i++;
-				if (i == mid_right) {
-						mid_rightp = cur;
-						break;
-				}
-				cur = cur->next;
-		}
-		mid_leftp = head;
-
-		while (mid_rightp) {
-				if (mid_leftp->val != mid_rightp->val)
+		for (int i = 0; i < ct; i++) {
+				if (buffer->val != cur->val)
 						return false;
-				mid_rightp = mid_rightp->next;
-				mid_leftp = mid_leftp->next;
+				buffer = buffer->next;
+				cur = cur->next;
+
 		}
 		return true;
-
 
 }
 
