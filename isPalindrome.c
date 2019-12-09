82615dca097b7d90cef38fafc3285586bb6a7d0f
xjk <2459169377@qq.com>
2019-12-09 11:23:52 +0800

    second edition

--- isPalindrome.c
+++ isPalindrome.c
@@ -6,7 +6,9 @@
  * };
  */
 
-
+/*
+// first go through the list and create a reverse list
+// second compare the half number of the two list
 bool isPalindrome(struct ListNode* head){
 		struct ListNode *buffer, *cur, *t;
 		int ct = 0, len = 0;
@@ -39,3 +41,34 @@ bool isPalindrome(struct ListNode* head){
 
 }
 
+*/
+bool isPalindrome(struct ListNode* head) {
+		struct ListNode *slow, *fast, *temp;
+		struct ListNode *pre_head = NULL;
+		slow = head;
+		fast = head;
+
+		if (head == NULL || head->next == NULL)
+				return true;
+		while (fast != NULL && fast->next != NULL) {
+				temp = slow;
+				slow = slow->next;
+				fast = fast->next->next;
+				temp->next = pre_head;
+				pre_head = temp;
+
+		}
+		if (fast == NULL)
+				;
+		else
+				slow = slow->next;
+
+		while (slow) {
+				if (slow->val != pre_head->val)
+						return false;
+				slow = slow->next;
+				pre_head = pre_head->next;
+		}
+
+		return true;
+}
