8e598d5f8650556fa1d6bd834d97fad0216379fd
xjk <2459169377@qq.com>
2019-12-21 09:36:19 +0800

    1. Two Sum
    hash map

--- twoSum.c
+++ twoSum.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+/*
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 		int i,j;
 		int *indices;
@@ -22,5 +23,47 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
 		}
 		*returnSize = 0;
 		return NULL;
+}
+*/
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+struct hash_map {
+		int key;
+		int index;
+		UT_hash_handle hh;
+};
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+		struct hash_map *myHashMap = NULL, *s;
+		int i, value;
+		int *retArr = NULL;
+		*returnSize = 0;
+		for (i = 0; i < numsSize; i++) {
+				value = target - nums[i];
+				HASH_FIND_INT(myHashMap, &value, s);
+				if (s == NULL) {
+						s = (struct hash_map *)malloc(sizeof(struct hash_map));
+						s->key = value;
+						HASH_ADD_INT(myHashMap, value, s);
+						s->index = i;
+				}
+
+				else {
+						retArr = (int *)malloc(2*sizeof(int));
+
+						retArr[0] = i;
+						retArr[1] = s->index;
+						*returnSize = 2;
+						break;
+				}
+		}
+
+		return retArr;
+
+
+
 }
 
