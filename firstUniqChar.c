93b4c92d9f58e4ec17d152fc51d5f4d3604cb6c1
xjk <2459169377@qq.com>
2019-12-26 09:33:35 +0800

    387.First Unique Character in a String.
    when and how to use many structures?

---
+++ firstUniqChar.c
@@ -0,0 +1,53 @@
+/**
+ * char -> times
+ * the record structure can be array or hash_map;
+ * */
+struct hash_map {
+		int c;
+		int count;
+		int index;
+
+		UT_hash_handle hh;
+};
+int firstUniqChar(char * s){
+		struct hash_map *myHashMap = NULL, *elem, *current, *tmp;
+		int val;
+		int index, i = 0;
+		int min_index = INT_MAX, best_c = -1;
+
+		for (i = 0; (val = s[i]) != '\0'; i++)
+		{
+				HASH_FIND_INT(myHashMap, &val, elem);
+				if (elem == NULL) {
+						elem = (struct hash_map*)malloc(sizeof(struct hash_map));
+						elem->c = val;
+						HASH_ADD_INT(myHashMap, c, elem);
+						elem->count = 1;
+						elem->index = i;
+
+				}
+
+				else {
+						elem->count++;
+				}
+		}
+		
+		HASH_ITER(hh, myHashMap, current, tmp) {
+				if (current->count == 1) {
+						if (min_index > current->index)
+						{
+								min_index = current->index;
+								best_c = current->c;
+						}
+				}
+
+		}
+
+		HASH_FIND_INT(myHashMap, &best_c, elem);
+		if (elem == NULL)
+				return -1;
+		else
+				return min_index;
+
+}
+
