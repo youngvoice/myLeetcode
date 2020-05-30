0bafad85bd285a9b65a2caf7de98a64c0a4079b2
xjk <2459169377@qq.com>
2020-05-30 23:25:25 +0800

    note the condition plus 1

--- plusOne.c
+++ plusOne.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+/**
 int* plusOne(int* digits, int digitsSize, int* returnSize){
 		int i,sum;
 		int carry = 1, overFlag = 0;
@@ -37,6 +38,41 @@ int* plusOne(int* digits, int digitsSize, int* returnSize){
 		}
 
 		return resultDigits;
+
+}
+*/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+	int sizeLarger = 1, i = 0;
+	int *result;
+	for (i = digitsSize - 1; i >= 0; i--) {
+		digits[i] = digits[i] + 1;
+		// if sum is 10, the carry is occur
+		if (digits[i] % 10 == 0) digits[i] = 0;
+		else {sizeLarger = 0; break;}
+	}
+
+	if (sizeLarger) {
+		//result = (int *)malloc(digitsSize + 1);
+		result = (int *)malloc((digitsSize + 1)*sizeof(int));
+		for (i = digitsSize - 1; i >= 0; i--) {
+			result[i+1] = digits[i];
+		}
+		//result[0] = result[1] / 10;
+        result[0] = 1;
+		result[1] = result[1] % 10;
+	}
+	else {
+		result = (int *)malloc(digitsSize*sizeof(int));
+		for (i = digitsSize - 1; i >= 0; i--) {
+			result[i] = digits[i];
+		}
+	}
+	*returnSize = digitsSize + sizeLarger;
+	return result;
 
 }
 
