56f3343c2bd59d858d4bd59decaf0b74ac29a863
xjk <2459169377@qq.com>
2020-06-06 22:50:47 +0800

    add (recursive + memo array) and DP

--- climbStairs_70.c
+++ climbStairs_70.c
@@ -1,4 +1,5 @@
 /* recursive*/
+/*
 int climbStairs(int n){
     // f(n) = f(n-1) + f(n-2)
     // f(1) = 1 , f(2) = 2
@@ -11,6 +12,8 @@ int climbStairs(int n){
     }
     else return climbStairs(n-1) + climbStairs(n-2);
 }
+*/
+
 /* circulation*/
 /*
 int climbStairs(int n){
@@ -44,3 +47,51 @@ int climbStairs(int n){
     }
 }
 */
+
+/* recursive + memo array*/
+
+/*
+struct memo_t{
+    char exit;
+    int value;
+};
+int climbStairs(int n){
+    // f(n) = f(n-1) + f(n-2)
+    // f(1) = 1 , f(2) = 2
+    //struct memo_t memo[n];
+    struct memo_t *memo = (struct memo_t *)malloc((n+1) * sizeof(struct memo_t));
+    memset(memo, 0, (n+1)*sizeof(struct memo_t));
+    if (n == 1) {
+        memo[1].exit = 1;
+        memo[1].value = 1;
+        return 1;
+    }
+    if (n == 2) {
+        memo[1].exit = 1;
+        memo[1].value = 2;
+        return 2;
+    }
+
+    if (memo[n].exit) return memo[n].value;
+    else {
+        memo[n].exit = 1;
+        memo[n].value = climbStairs(n-1) + climbStairs(n-2);
+        return memo[n].value;
+    }
+}
+
+*/
+
+/*
+* DP
+*/
+
+int climbStairs(int n){
+    int *dp = (int *)malloc((n+1)*sizeof(int));
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i < n + 1; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];    
+}
